0656d75ccf606aaa18879fb97132cf5dcecee716
Camden Blasingame <camden.blasingame@kearsarge.org>
2023-11-01 08:49:33 -0400

    Story 3

---
+++ Story3.py
@@ -0,0 +1,28 @@
+from Getters import *
+from Screen import *
+import os
+import time
+
+def Story3(debug = False):
+	if debug: print("Story 3 Function")
+	
+	z =  " ____________________________________ \n"
+	z += "|  HP: 10        ___                 |\n"
+	z += "|               /. .\\                |\n"
+	z += "|              [  v  ]               |\n"
+	z += "|               \\___/                |\n"
+	z += "|                /|\\                 |\n"
+	z += "|               / | \\                |\n"
+	z += "|              /  |  \\               |\n"
+	z += "|                 |                  |\n"
+	z += "|                 |                  |\n"
+	z += "|                / \\                 |\n"
+	z += "|               /   \\                |\n"	
+	z += "|              /     \\               |\n"
+	z += "|   +======+  |||||||||  +======+    |\n"
+	z += "|   |ATTACK|   \\\\\\|///   |DEFEND|    |\n"	
+	z += "|   +======+     [|]     +======+    |\n"
+	z += "|____________________________________|\n"
+
+	return z
+

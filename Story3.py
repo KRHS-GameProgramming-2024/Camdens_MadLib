a109a76303ac775cdcce3946ae718cf4b054cd23
Camden Blasingame <camden.blasingame@kearsarge.org>
2023-11-02 12:55:41 -0400

    RPG Nanner

--- Story3.py
+++ Story3.py
@@ -1,28 +1,29 @@
 from Getters import *
 from Screen import *
+from Story3getter import *
 import os
 import time
 
 def Story3(debug = False):
 	if debug: print("Story 3 Function")
 	
-	z =  " ____________________________________ \n"
-	z += "|  HP: 10        ___                 |\n"
-	z += "|               /. .\\                |\n"
-	z += "|              [  v  ]               |\n"
-	z += "|               \\___/                |\n"
-	z += "|                /|\\                 |\n"
-	z += "|               / | \\                |\n"
-	z += "|              /  |  \\               |\n"
-	z += "|                 |                  |\n"
-	z += "|                 |                  |\n"
-	z += "|                / \\                 |\n"
-	z += "|               /   \\                |\n"	
-	z += "|              /     \\               |\n"
-	z += "|   +======+  |||||||||  +======+    |\n"
-	z += "|   |ATTACK|   \\\\\\|///   |DEFEND|    |\n"	
-	z += "|   +======+     [|]     +======+    |\n"
-	z += "|____________________________________|\n"
+	print(Story3a(debug))
+	action = False
+	while action == False:
+		act = input("Attack or Defend, A/D: ")
+		os.system("cls")	
+		if act.lower() == "a":
+			print(Story3acttack(debug))
+			time.sleep(.2)
+			os.system("cls")
+			print(Story3b(debug))
+			action = True
+		elif act.lower() == "d":
+			print(Story3d(debug))
+			action = True
+		else:
+			print("ERROR")
+			action = False
+	
 
-	return z
 
